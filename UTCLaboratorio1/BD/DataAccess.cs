a6c83458525ed7f8ccde5da4bbb877bd68f3498b
Andres Rodriguez Morales <a-ndresrm@hotmail.com>
2022-02-16 19:24:00 -0600

    Clase 1

--- UTCLaboratorio1/BD/DataAccess.cs
+++ UTCLaboratorio1/BD/DataAccess.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,40 @@ namespace BD
 {
     public class DataAccess
     {
+        private readonly IConfiguration config;
+
+        //Constructor
+        public DataAccess(IConfiguration _Config)
+        {
+            //VARIABLE QUE ME PERMITE GUARDAR LA CONFIGURACION DE CONECCION A BD
+            config = _Config;
+        }
+
+        //METODO PARA REALIZAR LA CONECCION A LA BASE DE DATOS
+        public SqlConnection DbConection => new SqlConnection(
+            new SqlConnectionStringBuilder(config.GetConnectionString("Conn")).ConnectionString);
+
+        //REPRESENTACION DE RETORNO DE UNA LISTA
+        public async Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param= null, int? Timeout= null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        
+        }
+
+
     }
 }

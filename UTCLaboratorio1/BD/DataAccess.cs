3e46ccd5cefa1d08a175ef68543c1a3760c911ce
Andres Rodriguez Morales <a-ndresrm@hotmail.com>
2022-02-22 21:35:50 -0600

    Proyeto Db Listo

--- UTCLaboratorio1/BD/DataAccess.cs
+++ UTCLaboratorio1/BD/DataAccess.cs
@@ -1,4 +1,6 @@
 ﻿using Dapper;
+using Dapper.Mapper;
+using Entity;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace BD
 {
-    public class DataAccess
+    public class DataAccess : IDataAccess
     {
         private readonly IConfiguration config;
 
@@ -24,8 +26,8 @@ namespace BD
         public SqlConnection DbConection => new SqlConnection(
             new SqlConnectionStringBuilder(config.GetConnectionString("Conn")).ConnectionString);
 
-        //REPRESENTACION DE RETORNO DE UNA LISTA
-        public async Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param= null, int? Timeout= null)
+        //METODO DE RETORNO DE UNA LISTA
+        public async Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param = null, int? Timeout = null)
         {
 
             try
@@ -42,7 +44,197 @@ namespace BD
 
                 throw;
             }
-        
+
+        }
+
+        //METODO DE RETORNO DE UNA LISTA DYNAMICA
+        public async Task<IEnumerable<dynamic>> QueryAsync(string sp, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO DE RETORNO DE UNA LISTA DE UNA ENTIDAD PERO RELACIONADA A OTRA ENTIDAD(COMO UN TRIGGER)
+        public async Task<IEnumerable<T>> QueryAsync<T, B>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, B, C>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B, C>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, B, C, D>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B, C, D>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, B, C, D, E>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B, C, D, E>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B, C, D, E, F>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F, G>(string sp, string split, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryAsync<T, B, C, D, E, F, G>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout, splitOn: split);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO DE RETORNO DE UN SOLO REGISTRO
+        public async Task<T> QueryFirstAsync<T>(string sp, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = exec.QueryFirstOrDefaultAsync<T>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout);
+                    return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        //METODO DE RETORNo DE LA GESTION DE MENSAJES DE ERROR 
+        public async Task<DBEntity> ExecuteAsync(string sp, object Param = null, int? Timeout = null)
+        {
+
+            try
+            {
+                using (var exec = DbConection)
+                {
+                    await exec.OpenAsync();
+                    var result = await exec.ExecuteReaderAsync(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout);
+                    await result.ReadAsync();
+                    return new()
+                    {
+                        CodeError = result.GetInt32(0),
+                        MsgError = result.GetString(1)
+                    };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
 
 

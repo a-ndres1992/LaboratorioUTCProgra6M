ecec795ac1ef452935a7d4277a29a452b585aba8
Andres Rodriguez Morales <a-ndresrm@hotmail.com>
2022-02-25 19:02:25 -0600

    Clase 4

---
+++ UTCLaboratorio1/Entity/dbo/ProovedorEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ProovedorEntity : DBEntity
+    {
+
+        public int? IdProovedor{get; set;}
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Edad { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ UTCLaboratorio1/WBL/ProveedorService.cs
@@ -0,0 +1,156 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public interface IProveedorService
+    {
+        Task<DBEntity> Create(ProovedorEntity entity);
+        Task<DBEntity> Delete(ProovedorEntity entity);
+        Task<IEnumerable<ProovedorEntity>> Get();
+        Task<ProovedorEntity> GetById(ProovedorEntity entity);
+        Task<DBEntity> Update(ProovedorEntity entity);
+    }
+
+    public class ProveedorService : IProveedorService
+    {
+        private readonly IDataAccess sql;
+
+        public ProveedorService(IDataAccess _sql)
+        {
+            sql = _sql;  
+        }
+
+        #region MetodosCrud
+
+        //Metodo Get
+        public async Task<IEnumerable<ProovedorEntity>> Get()
+        {
+
+            try
+            {
+                var result = sql.QueryAsync<ProovedorEntity>("dbo.ProveedorObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO GET BY ID
+
+        public async Task<ProovedorEntity> GetById(ProovedorEntity entity)
+        {
+
+            try
+            {
+                var result = sql.QueryFirstAsync<ProovedorEntity>("dbo.ProveedorObtener", new { entity.IdProovedor });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO CREATE
+
+        public async Task<DBEntity> Create(ProovedorEntity entity)
+        {
+
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ProveedorInsertar", new
+                {
+                    entity.Identificacion
+                   ,
+                    entity.Nombre
+                   ,
+                    entity.PrimerApellido
+                   ,
+                    entity.SegundoApellido
+                   ,
+                    entity.Edad
+                   ,
+                    entity.FechaNacimiento
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO Update
+
+        public async Task<DBEntity> Update(ProovedorEntity entity)
+        {
+
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ProveedorActualizar", new
+                {
+                    entity.IdProovedor
+                   ,
+                    entity.Identificacion
+                   ,
+                    entity.Nombre
+                   ,
+                    entity.PrimerApellido
+                   ,
+                    entity.SegundoApellido
+                   ,
+                    entity.Edad
+                   ,
+                    entity.FechaNacimiento
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //METODO DELETE
+
+        public async Task<DBEntity> Delete(ProovedorEntity entity)
+        {
+
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ProveedorEliminar", new
+                {
+                    entity.IdProovedor
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
+

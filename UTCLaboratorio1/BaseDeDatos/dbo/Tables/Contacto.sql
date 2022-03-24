a4b0e61b251a078f9bb75533c93936a8c55eda3a
Andres Rodriguez Morales <a-ndresrm@hotmail.com>
2022-03-23 18:53:59 -0600

    Tabla Contacto

---
+++ UTCLaboratorio1/BaseDeDatos/dbo/Tables/Contacto.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Contacto]
+(
+	IdContacto INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Contacto PRIMARY KEY CLUSTERED,
+	IdProveedor INT NOT NULL CONSTRAINT FK_Contacto_Proveedor FOREIGN KEY(IdProveedor) REFERENCES dbo.Proveedor(IdProveedor),
+	Identificacion VARCHAR(250) NOT NULL,
+    Nombre VARCHAR(250) NOT NULL,
+	PrimerApellido VARCHAR(250) NOT NULL,
+	SegundoApellido VARCHAR(250) NOT NULL,
+
+)
+WITH (DATA_COMPRESSION = page)
+GO
